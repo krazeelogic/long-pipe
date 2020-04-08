0e45fa95b3209efe26407e02f9485c322d98f183
Khurram Munir Khokhar <sw.engineer@gmail.com>
2020-04-09 03:18:38 +0500

    bit more

--- app.js
+++ app.js
@@ -5,12 +5,36 @@ const express = require('express')
 const hostname = '127.0.0.1';
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
+  //console.log(`KK\n`);
 });
 
+
+/*
+const server = http.get({
+    hostname: 'localhost',
+    port: 80,
+    path: '/kk',
+    agent: false  // Create a new agent just for this one request
+  }, (res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Khurram Khokhar');
+    });
+*/
+
+
+// App
+//const app = express();
+//app.get('/', (req, res) => {
+  //res.send('Hello World');
+  //console.log(`KK\n`);
+//});
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`KK Server running at http://${hostname}:${port}/`);
 });
